0f297a312fa5502b716f56aa7f0449a1635ce65b
samglvn <samanglvn11@gmail.com>
2018-06-21 10:55:58 -0500

    prueba2

---
+++ app.js
@@ -0,0 +1,5 @@
+var age = parseInt(prompt ('¿cual es tu edad?'));
+//convertir la edad en segundos
+var ageInseconds = age*365*24*60*60;
+//mostrar la edad en la web
+document.write('Tu edad es '+ age + ' y en segundos sería ' + ageInseconds);
